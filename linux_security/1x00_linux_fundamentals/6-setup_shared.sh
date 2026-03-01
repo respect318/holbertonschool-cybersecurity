c432e517027d28cd29d2e27129335bb952adbc33
testfarid <11293@holbertonstudents.com>
2026-03-01 14:37:20 -0500

    task

--- linux_security/1x00_linux_fundamentals/6-setup_shared.sh
+++ linux_security/1x00_linux_fundamentals/6-setup_shared.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p "$1" && chown :developers "$1" && chmod 2775 "$1" && chmod +t "$1"
+mkdir -p /shared/devs && chown root:developers /shared/devs && chmod 2770 /shared/devs && chmod +t /shared/devs
 
