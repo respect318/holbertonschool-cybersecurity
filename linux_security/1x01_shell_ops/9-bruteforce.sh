3291ab6369a8eb56dcb637acdedcc99ea543b71c
testfarid <11293@holbertonstudents.com>
2026-03-02 01:40:52 -0500

    test

--- linux_security/1x01_shell_ops/9-bruteforce.sh
+++ linux_security/1x01_shell_ops/9-bruteforce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep "Failed password" "$1" | awk '{for(i=1;i<=NF;i++) if ($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $i}' | sort | uniq -c | sort -nr | awk '{$1=$1; print}'
+grep "Failed password" "$1" | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr | awk '{$1=$1; print}'
 
