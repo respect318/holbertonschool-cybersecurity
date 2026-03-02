ca86f32759a90c7993492e5d8c40a69e1053902c
testfarid <11293@holbertonstudents.com>
2026-03-02 02:03:28 -0500

    test

--- linux_security/1x01_shell_ops/11-sqli_report.sh
+++ linux_security/1x01_shell_ops/11-sqli_report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep "$2" "$1" | sed -n 's/.*\[\([^]]*\)\].*from \([0-9.]*\).*/\1,\2/p' | sort > "$3"
+grep -i 'UNION SELECT' /var/log/analysis/access.log | awk '{print substr($4,2) "," $1}' | sort -t, -k2,2 > sqli_report.csv
 
