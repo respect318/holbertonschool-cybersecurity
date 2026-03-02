f334eff676202199188e119e667b34b527271842
testfarid <11293@holbertonstudents.com>
2026-03-02 01:51:37 -0500

    test

--- linux_security/1x01_shell_ops/10-decode.sh
+++ linux_security/1x01_shell_ops/10-decode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep "$2" "$1" | sed -n 's/.*cmd=\([^&]*\).*/\1/p' | sed 's/%3D/=/' | base64 --decode > "$3"
+grep "$2" "$1" | sed -n 's/.*cmd=\([^&]*\).*/\1/p' | sed 's/%3D/=/' | base64 -d > "$3"
 
