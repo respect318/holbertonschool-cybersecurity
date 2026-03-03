3e8bfcd21380e3e2483bfe2bc828b4dfcfe1c27c
testfarid <11293@holbertonstudents.com>
2026-03-03 12:10:42 -0500

    test

--- linux_security/1x02_identity_management/7-sudo_config.sh
+++ linux_security/1x02_identity_management/7-sudo_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cat <<EOF > /etc/sudoers.d/junior
-$1 ALL=(ALL) /usr/bin/systemctl restart apache2, /usr/bin/journalctl
+$1 ALL=(root) /usr/bin/systemctl restart apache2, /usr/bin/journalctl
 EOF
 chmod 440 /etc/sudoers.d/junior
 visudo -c
