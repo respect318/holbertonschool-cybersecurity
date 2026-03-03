273e5af0949a1540335e13e2fef70a547b0358e1
testfarid <11293@holbertonstudents.com>
2026-03-03 11:12:24 -0500

    test

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -F: '$3>=1000 && $1!="nobody" {print $1}' "$1" | while read u; do id -Gn "$u" | grep -owE "disk|docker|shadow" | sed "s/^/$u:/"; done
+awk -F: '$3>=1000 && $1!="nobody" {print $1}' "$1" | while read u; do for g in disk docker shadow; do id -Gn "$u" | grep -qw "$g" && echo "$u:$g"; done; done
