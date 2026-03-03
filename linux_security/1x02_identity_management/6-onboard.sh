7d30586d08e3d58cc5be8ab39ee8f5563f321efd
testfarid <11293@holbertonstudents.com>
2026-03-03 11:53:34 -0500

    test

---
+++ linux_security/1x02_identity_management/6-onboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+useradd -m "$1"
+passwd -l "$1"
+mkdir -p /home/"$1"/.ssh
+echo "$2" > /home/"$1"/.ssh/authorized_keys
+chmod 700 /home/"$1"/.ssh
+chmod 600 /home/"$1"/.ssh/authorized_keys
+chown -R "$1":"$1" /home/"$1"/.ssh
