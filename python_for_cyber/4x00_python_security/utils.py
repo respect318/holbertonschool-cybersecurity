4c040d36bf7d18b5b7d9710dc1b5e4f08eaa61bf
testfarid <11293@holbertonstudents.com>
2026-03-21 03:18:52 -0400

    python

--- python_for_cyber/4x00_python_security/utils.py
+++ python_for_cyber/4x00_python_security/utils.py
@@ -2,6 +2,7 @@
 import re
 import hashlib
 
+
 def clean_data(lines: list) -> list:
     cleaned_lines = []
     for line in lines:
@@ -10,9 +11,11 @@ def clean_data(lines: list) -> list:
             cleaned_lines.append(stripped_line)
     return cleaned_lines
 
+
 def validate_line(line: str) -> bool:
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+:.+$"
     return bool(re.match(pattern, line))
+
 
 def hash_password(password: str, salt: str) -> str:
     salted_password = password + salt
