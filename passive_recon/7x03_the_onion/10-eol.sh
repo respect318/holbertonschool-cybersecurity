b54dd22b21eba6b3a508a2d71bfdc6eec5584dfa
respect318 <ferid.mirzezade.education@gmail.com>
2026-07-10 04:19:34 -0400

    PassiveRecon

--- passive_recon/7x03_the_onion/10-eol.sh
+++ passive_recon/7x03_the_onion/10-eol.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-
 FILES="1-declared_stack.txt 2-frontend.sh 3-proxy.sh 4-backend.sh 5-spoofing.txt 6-version.sh 8-framework.txt 9-datalayer.txt"
-
 COMPONENTS=$(cat $FILES 2>/dev/null | grep -oE '[a-zA-Z0-9_-]+/[0-9]+\.[0-9]+' | sort | uniq)
-
-OLDEST_DATE="9999-99-99"
-MOST_OUTDATED_COMP=""
-
+critical_date="9999-99-99"
+critical_comp=""
 for comp_ver in $COMPONENTS; do
     COMP_NAME=$(echo "$comp_ver" | cut -d'/' -f1)
     COMP_VER=$(echo "$comp_ver" | cut -d'/' -f2)
-    
     RESPONSE=$(curl -s "https://endoflife.date/api/${COMP_NAME}/${COMP_VER}.json")
-    
-    # JSON-u jq ile oxuyuruq
     EOL_DATE=$(echo "$RESPONSE" | jq -r '.eol')
-    
     if [[ -n "$EOL_DATE" && "$EOL_DATE" != "false" && "$EOL_DATE" != "null" ]]; then
-        if [[ "$EOL_DATE" < "$OLDEST_DATE" ]]; then
-            OLDEST_DATE="$EOL_DATE"
-            MOST_OUTDATED_COMP="$comp_ver"
+        if [[ "$EOL_DATE" < "$critical_date" ]]; then
+            critical_date="$EOL_DATE"
+            critical_comp="$comp_ver"
         fi
     fi
 done
-
-if [ -n "$MOST_OUTDATED_COMP" ]; then
-    echo "$MOST_OUTDATED_COMP"
-    echo "$OLDEST_DATE"
+if [ -n "$critical_comp" ]; then
+    echo "$critical_comp"
+    echo "$critical_date"
 fi
